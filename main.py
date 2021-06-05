0e65b8ffbf68206bb0bd1da82a46f2fd254e6902
hindBK <2020.helkhamlichielboukhari@suptem.com>
2021-06-05 14:58:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+# intialize the pygame
+pygame.init()
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runnimg = False
+
+
