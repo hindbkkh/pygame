1aa8f0221868d1beeca0913b62d8ab5bddc29831
hindBK <2020.helkhamlichielboukhari@suptem.com>
2021-06-05 20:10:58 +0100

    TITLE AND ICON

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-running = True
+#Title and Icon
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('battleship.png')
+pygame.display.set_icon(icon)
+
+
 # Game Loop
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            runnimg = False
-            
+            running = False
+
 
