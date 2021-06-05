80232d90b8584f759725f13a31f447f6557c7d34
hindBK <2020.helkhamlichielboukhari@suptem.com>
2021-06-05 15:24:49 +0100

    CREATING THE GAME WINDOW

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 running = True
-
+# Game Loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runnimg = False
+            
 
