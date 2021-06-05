4d03da96ff70ec08f8a32568d8324d20b2dba1d5
hindBK <2020.helkhamlichielboukhari@suptem.com>
2021-06-05 20:45:12 +0100

    ADDING IMAGES

--- main.py
+++ main.py
@@ -5,18 +5,34 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-#Title and Icon
+# Title and Icon
 
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('battleship.png')
 pygame.display.set_icon(icon)
 
+# player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
+
 
 # Game Loop
 running = True
 while running:
+
+    # RGB =Red, Green , Blue
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
 
+
+        player()
+        pygame.display.update()
